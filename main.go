219b53ce6ad4055c6c4da583107407dc06058cc9
Jared Babinec <jared@krafton.com>
2022-01-23 18:55:52 -0500

    add main file

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+ 
+	"github.com/jbabinec1/hello-world-test-service/server"
+	"github.com/jbabinec1/hello-world-test-service/rpc"
+
+)
+
+func main() {
+	fmt.Printf("Starting Service on :6666")
+
+	server := server.NewServer()
+	twirpHandler := rpc.NewAccountsService(server, nil)
+    fmt.Println(twirpHandler)
+	log.Fatal(http.ListenAndServe(":6666", twirpHandler))
+}
