9f8f5b1b3b05b55e6d222ed5e425dc259579e806
Jared Babinec <jared@krafton.com>
2022-01-25 16:56:04 -0500

    adjust server functions

--- server/server.go
+++ server/server.go
@@ -21,14 +21,14 @@ func NewServer() *Server {
 }
 
 
-func (s *Server) IsAccountLinked(ctx context.Context, req *pb.AccountLookupRequest) (*pb.AccountLookupResponse, error) {
+func (s *Server) IsAccountLinked(ctx context.Context, *pb.AccountLookupRequest) (*pb.AccountLookupResponse, error) {
     return &pb.AccountLookupResponse{
         AccountID: "jared",
     }, nil
 }
 
 
-func (s *Server) PushToAccount(ctx context.Context, request *pb.AccountDropsRequest) (*pb.AccountDropsResponse, error) {
+func (s *Server) PushToAccount(ctx context.Context, *pb.AccountDropsRequest) (*pb.AccountDropsResponse, error) {
     return &pb.AccountDropsResponse {
 		Item: "noob_weapon",
 	}, nil
